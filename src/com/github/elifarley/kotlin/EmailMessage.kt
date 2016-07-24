232224dd9cf4f88fd954332412be445b680925cb
Elifarley C <elifarley@users.noreply.github.com>
2016-07-24 19:28:42 -0300

    Update EmailMessage.kt

--- src/com/github/elifarley/kotlin/EmailMessage.kt
+++ src/com/github/elifarley/kotlin/EmailMessage.kt
@@ -2,4 +2,4 @@ package com.github.elifarley.kotlin
 
 import java.io.Serializable
 
-class EmailMessage(val toAddress: String, val subject: String, val body: String, var myMutableField: String?) : Serializable
+data class EmailMessage(val toAddress: String, val subject: String, val body: String, var myMutableField: String?) : Serializable
