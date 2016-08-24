131b7773a839ad23c7529ed93204c1be95ce7a37
Elifarley C <elifarley@users.noreply.github.com>
2016-08-24 18:02:26 -0300

    Refactoring

--- src/com/github/elifarley/kotlin/DirWatcherBean.kt
+++ src/com/github/elifarley/kotlin/DirWatcherBean.kt
@@ -1,31 +1,35 @@
 package inotify
 
-import inotify.PathHandler
-import inotify.watchDir
 import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 import javax.annotation.PostConstruct
 import javax.annotation.PreDestroy
 
+@JvmOverloads
 fun dirWatcherBean(dir: String, pathHandler: PathHandler,
-                   sleepInMillis: Long = 5000, executorService: ExecutorService) = object {
+                   sleepInMillis: Long = 5000, executorService: ExecutorService = Executors.newFixedThreadPool(2)
+) = object {
+    // Packages and file facades are not yet supported in Kotlin reflection. Meanwhile please use Java reflection to inspect DirWatcherBeanKt
+
+    private val LOG = logger(DirectoryWatcher.javaClass)
+
     @PostConstruct
     fun init() {
-        val dirWatcher = "/tmp/watch".watchDir(executorService)
-        executorService.execute({ dirWatcher.loop(pathHandler, sleepInMillis) })
-        executorService.shutdown()
+        val dirWatcher = dir.watchDirLoop( { pathHandler.handle(it) }, sleepInMillis, executorService)
     }
 
     @PreDestroy
-    fun beanDestroy() {
-        if (executorService == null) return
+    fun beanDestroy() =
         try {
+            LOG.warn("[dirWatcherBean PreDestroy START]")
             // wait 1 second for closing all threads
             executorService.awaitTermination(1, TimeUnit.SECONDS)
+            LOG.warn("[dirWatcherBean PreDestroy END]")
+
         } catch (e: InterruptedException) {
+            LOG.warn("[dirWatcherBean INTERRUPTED]")
             Thread.currentThread().interrupt()
         }
-
-    }
 
 }
