7384fdd22bef1e010fa13ee977a0a3da3cf59a5c
Elifarley C <elifarley@users.noreply.github.com>
2016-11-15 15:13:49 -0200

    Create PersistenceKit.kt

---
+++ src/com/github/elifarley/kotlin/persistence/PersistenceKit.kt
@@ -0,0 +1,15 @@
+package com.m4u.cielo.utils
+
+import java.sql.Date
+import java.sql.Timestamp
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+fun toConvertedArrayParams(params: Collection<Any?>) = params.toTypedArray().apply {
+    this.forEachIndexed { i, element ->
+        when (element) {
+            is LocalDate -> this[i] = Date.valueOf(element)
+            is LocalDateTime -> this[i] = Timestamp.valueOf(element)
+        }
+    }
+}
