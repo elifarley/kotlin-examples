e0fe5356a368552e832df94019daad84dbce0dff
Elifarley C <elifarley@users.noreply.github.com>
2016-12-01 18:18:23 -0200

    Refactoring

--- src/com/github/elifarley/kotlin/DirWatcherBean.kt
+++ src/com/github/elifarley/kotlin/DirWatcherBean.kt
@@ -1,5 +1,6 @@
-package inotify
+package com.orgecc.util.file
 
+import com.orgecc.util.WithLogging
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
@@ -9,14 +10,12 @@ import javax.annotation.PreDestroy
 @JvmOverloads
 fun dirWatcherBean(dir: String, pathHandler: PathHandler,
                    sleepInMillis: Long = 5000, executorService: ExecutorService = Executors.newFixedThreadPool(2)
-) = object {
+) = object : WithLogging() {
     // Packages and file facades are not yet supported in Kotlin reflection. Meanwhile please use Java reflection to inspect DirWatcherBeanKt
-
-    private val LOG = logger(DirectoryWatcher.javaClass)
 
     @PostConstruct
     fun init() {
-        val dirWatcher = dir.watchDirLoop( { pathHandler.handle(it) }, sleepInMillis, executorService)
+        dir.watchDirLoop( { pathHandler.handle(it) }, sleepInMillis, executorService)
     }
 
     @PreDestroy
