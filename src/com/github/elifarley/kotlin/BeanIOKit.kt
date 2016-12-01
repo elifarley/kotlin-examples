cf8542c9590b1437a9d78b7770fcb769936b9b43
Elifarley C <elifarley@users.noreply.github.com>
2016-12-01 18:26:14 -0200

    Update BeanIOKit.kt

--- src/com/github/elifarley/kotlin/BeanIOKit.kt
+++ src/com/github/elifarley/kotlin/BeanIOKit.kt
@@ -1,14 +1,25 @@
-package com.orgecc.io
+package com.orgecc.util.io
 
+import com.m4u.util.io.IOWithDigest
+import com.m4u.util.io.digestInputStream
+import com.m4u.util.io.digestOutputStream
 import org.beanio.BeanReader
 import org.beanio.BeanWriter
 import org.beanio.StreamFactory
-import java.io.Closeable
+import java.io.Reader
+import java.io.Writer
 import java.nio.file.Path
+import java.security.MessageDigest
 
 /**
  * Created by elifarley on 24/08/16.
  */
+
+fun useNewBeanIOReaderPreKotlin1dot1(streamName: String, filePath: Path, block: (IOWithDigest.MDReader) -> Unit) =
+        BeanIOKit.useNewReader(streamName, filePath, block)
+fun useNewBeanIOWriterPreKotlin1dot1(streamName: String, filePath: Path, block: (IOWithDigest.MDWriter) -> Unit) =
+        BeanIOKit.useNewWriter(streamName, filePath, block)
+
 object BeanIOKit {
 
     private val streamFactory: StreamFactory by lazy {
@@ -17,24 +28,35 @@ object BeanIOKit {
         }
     }
 
-    fun useNewReader(streamName: String, filePath: Path, block: (BeanReader) -> Unit) =
-            streamFactory.createReader(streamName, filePath.toFile()).useAsCloseable(block)
+    fun useNewReader(streamName: String, reader: Reader, digest: MessageDigest, block: (IOWithDigest.MDReader) -> Unit) =
+            streamFactory.createReader(streamName, reader).useAsCloseable(digest, block)
 
-    fun useNewWriter(streamName: String, filePath: Path, block: (BeanWriter) -> Unit) =
-            streamFactory.createWriter(streamName, filePath.toFile()).useAsCloseable(block)
+    fun useNewWriter(streamName: String, writer: Writer, digest: MessageDigest, block: (IOWithDigest.MDWriter) -> Unit) =
+            streamFactory.createWriter(streamName, writer).useAsCloseable(digest, block)
+
+    fun useNewReader(streamName: String, filePath: Path, block: (IOWithDigest.MDReader) -> Unit) {
+        val mdStream = filePath.toFile().digestInputStream()
+        mdStream.bufferedReader().use {
+            useNewReader(streamName, it, mdStream.messageDigest, block)
+
+        }
+    }
+
+    fun useNewWriter(streamName: String, filePath: Path, block: (IOWithDigest.MDWriter) -> Unit) {
+        val mdStream = filePath.toFile().digestOutputStream()
+        mdStream.bufferedWriter().use {
+            useNewWriter(streamName, it, mdStream.messageDigest, block)
+        }
+    }
 
 }
 
-private class CloseableBeanReader(val b: BeanReader) : BeanReader by b, Closeable {
-    override final inline fun close() = b.close()
-}
+private class CloseableBeanReaderWithDigest(b: BeanReader, md: MessageDigest)
+    : BeanReader by b, IOWithDigest.MDReader(md)
+private class CloseableBeanWriterWithDigest(b: BeanWriter, md: MessageDigest)
+    : BeanWriter by b, IOWithDigest.MDWriter(md)
 
-private inline fun BeanReader.useAsCloseable(block: (BeanReader) -> Unit) =
-        CloseableBeanReader(this).use(block)
-
-private class CloseableBeanWriter(val b: BeanWriter) : BeanWriter by b, Closeable {
-    override final inline fun close() = b.close()
-}
-
-private inline fun BeanWriter.useAsCloseable(block: (BeanWriter) -> Unit) =
-        CloseableBeanWriter(this).use(block)
+private inline fun BeanReader.useAsCloseable(digest: MessageDigest, block: (IOWithDigest.MDReader) -> Unit) =
+        CloseableBeanReaderWithDigest(this, digest).use(block)
+private inline fun BeanWriter.useAsCloseable(digest: MessageDigest, block: (IOWithDigest.MDWriter) -> Unit) =
+        CloseableBeanWriterWithDigest(this, digest).use(block)
