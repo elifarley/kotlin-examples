9526000ca0ad747eb8da678f26e165f862a67612
Elifarley C <elifarley@users.noreply.github.com>
2016-12-01 18:17:31 -0200

    Refactoring

--- src/com/github/elifarley/kotlin/DirectoryWatcher.kt
+++ src/com/github/elifarley/kotlin/DirectoryWatcher.kt
@@ -1,5 +1,8 @@
-package inotify
+package com.orgecc.util.file
 
+import com.m4u.util.WithLogging
+import com.m4u.util.logger
+import java.io.File
 import java.io.IOException
 import java.nio.file.*
 import java.util.concurrent.ConcurrentLinkedQueue
@@ -10,17 +13,46 @@ interface PathHandler {
     fun handle(path: Path)
 }
 
-fun Path.handleExistingFiles(pathHandler: (Path) -> Unit) =
-        Files.newDirectoryStream(this) { entry -> !Files.isDirectory(entry) }.use { directoryStream ->
-            for (path in directoryStream) {
-                pathHandler(path)
+fun Path.handleAndDelete(pathHandler: (Path) -> Unit): Unit =
+        try {
+            DirectoryWatcher.LOG.warn("Will handle '$this'")
+            pathHandler(this)
+            DirectoryWatcher.LOG.debug("Done. Deleting '$this'...")
+
+            try {
+                Files.delete(this)
+
+            } catch(e: Exception) {
+                DirectoryWatcher.LOG.error("Unable to delete '$this'")
             }
+
+        } catch (e: Exception) {
+            logger(pathHandler.javaClass).error("Unable to handle file '$this'", e)
+
+            this.parent.resolveSibling("error").let { errorPath ->
+                Files.createDirectories(errorPath)
+                var errorFile = errorPath.resolve(this.fileName)
+                var tries = 0
+                while (tries  < 1000 && Files.exists(errorFile)) {
+                    tries++
+                    errorFile = File("$errorFile.$tries").toPath()
+                }
+                Files.move(this, errorFile)
+            }
+        }
+
+fun Path.enqueueExistingFiles(target: MutableCollection<Path>) =
+        Files.newDirectoryStream(this) {
+            !Files.isDirectory(it) &&
+                    !it.fileName.toString().startsWith(".")
+        }.use { directoryStream ->
+            target += directoryStream
         }
 
 // Simple class to watch directory events.
 class DirectoryWatcher(rootPathArg: String, executor: ExecutorService = Executors.newSingleThreadExecutor()) {
 
-    companion object : WithLogging() {}
+    companion object : WithLogging()
 
     private val rootPath = Paths.get(rootPathArg)
     private val queue = ConcurrentLinkedQueue<Path>()
@@ -31,9 +63,8 @@ class DirectoryWatcher(rootPathArg: String, executor: ExecutorService = Executor
     }
 
     val next: Path?
-        get() {
-            val result = queue.poll()
-            return if (result == null) null else rootPath.resolve(result)
+        get() = queue.poll()?.let {
+            rootPath.resolve(it)
         }
 
     fun waitNext(sleepInMillis: Long = 5000): Path {
@@ -47,10 +78,12 @@ class DirectoryWatcher(rootPathArg: String, executor: ExecutorService = Executor
 
     fun loop(pathHandler: (Path) -> Unit, sleepInMillis: Long = 5000) {
         while (!Thread.currentThread().isInterrupted) {
-            pathHandler(waitNext(sleepInMillis))
+            waitNext(sleepInMillis).handleAndDelete(pathHandler)
+
         }
 
     }
+
 }
 
 fun String.watchDir(executor: ExecutorService = Executors.newSingleThreadExecutor())
@@ -63,27 +96,29 @@ fun Path.enqueuePaths(eventQueue: ConcurrentLinkedQueue<Path>,
                       sleepInMillis: Long = 500, kind: WatchEvent.Kind<Path> = StandardWatchEventKinds.ENTRY_CREATE): Unit {
 
     Thread.currentThread().name = "enqueuePaths[$kind:${this.toAbsolutePath()}]"
+
+    Files.createDirectories(this)
+
     try {
 
-        this.handleExistingFiles { eventQueue.add(it) }
+        this.enqueueExistingFiles(eventQueue)
 
         val watchService = this@enqueuePaths.fileSystem.newWatchService()
         this@enqueuePaths.register(watchService, kind)
 
         // loop forever to watch directory
         while (!Thread.currentThread().isInterrupted) {
+
             val watchKey: WatchKey
             watchKey = watchService.take() // this call is blocking until events are present
 
-            for (event in watchKey.pollEvents()) {
-                val path = event.context() as Path
-                while (!eventQueue.offer(path)) Thread.sleep(sleepInMillis)
+            watchKey.pollEvents().map { it.context() as Path }.forEach {
+                while (!eventQueue.offer(it)) Thread.sleep(sleepInMillis)
             }
-
 
             // if the watched directed gets deleted, break loop
             if (!watchKey.reset()) {
-                DirectoryWatcher.LOG.warn("No longer valid")
+                DirectoryWatcher.LOG.warn("[enqueuePaths] No longer valid")
                 watchKey.cancel()
                 watchService.close()
                 break
@@ -135,20 +170,4 @@ fun Path.enqueueEvents(eventQueue: ConcurrentLinkedQueue<WatchEvent<*>>,
         DirectoryWatcher.LOG.warn("", ex)
     }
 
-}
-
-object FileEventTest {
-
-    @JvmStatic fun main(vararg args: String) {
-
-        var p: Path? = null
-        var i: Int = 0
-
-        val dirWatcher = "/tmp/watch".watchDir()
-        while (i++ < 10) {
-            p = dirWatcher.waitNext()
-            DirectoryWatcher.LOG.warn(i.toString() + ": " + p.toString())
-        }
-
-    }
 }
