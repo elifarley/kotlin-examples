19bab7a9ac922fb8fb789774dabea83d500c6cb0
Elifarley C <elifarley@users.noreply.github.com>
2017-07-20 13:53:24 -0300

    New extension function: ByteArray.toHex

--- src/com/github/elifarley/kotlin/MiscKit.kt
+++ src/com/github/elifarley/kotlin/MiscKit.kt
@@ -8,7 +8,7 @@ import org.apache.commons.lang3.time.StopWatch
 fun StopWatch.stopIfRunning(): StopWatch { if (!isStopped) stop(); return this }
 
 
-private val HEX_CHARS = arrayOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f')
+private val HEX_CHARS = "0123456789ABCDEF".toCharArray()
 
 /**
  *  Returns the string of two characters representing the HEX value of the byte.
@@ -16,7 +16,7 @@ private val HEX_CHARS = arrayOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
 fun Byte.toHexString() : String {
     val i = this.toInt()
     val char2 = HEX_CHARS[i and 0x0f]
-    val char1 = HEX_CHARS[i shr 4 and 0x0f]
+    val char1 = HEX_CHARS[i ushr 4]
     return "$char1$char2"
 }
 
@@ -26,4 +26,19 @@ fun ByteArray.toHexString() : String {
         builder.append(b.toHexString())
     }
     return builder.toString()
+}
+
+
+fun ByteArray.toHex(): String {
+    val result = StringBuffer(this.size * 2)
+
+    forEach {
+        val octet = it.toInt()
+        val firstIndex = (octet and 0xF0).ushr(4)
+        val secondIndex = octet and 0x0F
+        result.append(HEX_CHARS[firstIndex])
+        result.append(HEX_CHARS[secondIndex])
+    }
+
+    return result.toString()
 }
