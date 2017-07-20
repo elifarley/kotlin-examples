5caf959ea2ca4a66d00a2562ecc59cfe3c16bd2e
Elifarley C <elifarley@users.noreply.github.com>
2017-07-20 14:09:18 -0300

    Update CoroutineKit.kt

--- src/com/github/elifarley/kotlin/coroutines/CoroutineKit.kt
+++ src/com/github/elifarley/kotlin/coroutines/CoroutineKit.kt
@@ -1,5 +1,10 @@
 package com.github.elifarley.kotlin.coroutines
 
+import kotlinx.coroutines.experimental.Deferred
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.delay
+import kotlin.coroutines.experimental.CoroutineContext
+
 fun <T> delayWhile(
         predicate: T.() -> Boolean,
         maxDelayPerItem: Long = 30000L,
