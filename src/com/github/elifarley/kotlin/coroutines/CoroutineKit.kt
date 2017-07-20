91a6d18b276bd5d8d166157dcdf80a6e8bfe6e39
Elifarley C <elifarley@users.noreply.github.com>
2017-07-20 14:52:48 -0300

    New parameter: maxElapsedTime

--- src/com/github/elifarley/kotlin/coroutines/CoroutineKit.kt
+++ src/com/github/elifarley/kotlin/coroutines/CoroutineKit.kt
@@ -6,18 +6,22 @@ import kotlinx.coroutines.experimental.delay
 import kotlin.coroutines.experimental.CoroutineContext
 
 fun <T> delayWhile(
-        predicate: T.() -> Boolean,
-        maxDelayPerItem: Long = 30000L,
         context: CoroutineContext,
+        predicate: (T.() -> Boolean) = {this == null},
+        maxElapsedTime: Long = 2 * 60 * 1000,
+        maxDelayPerIter: Long = 30000L,
         block: suspend () -> T
 ): Deferred<T> = async(context) {
     val start = System.currentTimeMillis()
-    var result : T?
+    var result: T?
     while (true) {
         result = block()
         println("[delayWhile] ${System.currentTimeMillis() - start} ms; Predicate: ${result?.predicate() ?: true}") // TODO Remove
-        if (! (result?.predicate() ?: true)) break
-        delay(Math.min(maxDelayPerItem, System.currentTimeMillis() - start))
+        if (!(result?.predicate() ?: true)) break
+        if (System.currentTimeMillis() - start >= maxElapsedTime) {
+            throw TimeoutException("Max elapsed time exceeded. Elapsed time: ${System.currentTimeMillis() - start}ms")
+        }
+        delay(Math.min(maxDelayPerIter, System.currentTimeMillis() - start))
     }
     result!!
 }
