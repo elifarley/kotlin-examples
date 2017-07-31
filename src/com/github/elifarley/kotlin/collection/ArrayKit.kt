19ee2575e087c67b31d2ee4780904a1660a4cae7
Elifarley C <elifarley@users.noreply.github.com>
2017-07-31 18:33:31 -0300

    Update ArrayKit.kt

--- src/com/github/elifarley/kotlin/collection/ArrayKit.kt
+++ src/com/github/elifarley/kotlin/collection/ArrayKit.kt
@@ -1,3 +1,9 @@
-fun <T> Array<out T>.toPairs() = if (isEmpty()) emptyArray() else Array(this.size shr 1) { i ->
-    Pair(this[i * 2], this[i * 2 + 1])
+package com.orgecc.lib.collections
+
+/**
+ * Created by ecc on 7/30/17.
+ */
+
+fun <T> Array<out T>.toPairs() = if (isEmpty()) emptyArray() else Array(this.size shr 1) {
+    Pair(this[it * 2], this[it * 2 + 1])
 }
