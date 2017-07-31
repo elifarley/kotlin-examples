fdc680aaefbb73339107810b1e17aee2b524bea5
Elifarley C <elifarley@users.noreply.github.com>
2017-07-31 18:37:34 -0300

    Update AHCClientAdapter.kt

--- src/com/github/elifarley/kotlin/http/AHCClientAdapter.kt
+++ src/com/github/elifarley/kotlin/http/AHCClientAdapter.kt
@@ -32,27 +32,25 @@ class AHCClientAdapter : HttpClientAdapter<AsyncHttpClient, BoundRequestBuilder>
 
     }
 
-    private fun resultHandler(resultType: RESULT_TYPE, callback: (Any?) -> Unit): AsyncCompletionHandler<Response> =
-            object : AsyncCompletionHandler<Response>() {
+    private fun resultHandler(resultType: RESULT_TYPE, callback: (Any?) -> Unit) = object : AsyncCompletionHandler<Response>() {
 
-                override fun onCompleted(response: Response): Response {
+        override fun onThrowable(t: Throwable) {
+            t.printStackTrace() // TODO
+        }
+
+        override fun onCompleted(response: Response) = response.also {
                     when (resultType) {
                         RESULT_TYPE.BYTE_ARRAY -> callback(response.responseBodyAsBytes)
                         RESULT_TYPE.STRING -> callback(response.responseBody)
-                        RESULT_TYPE.JSON -> response.responseBodyAsBytes?.let { JsonIterator.deserialize(it) }
-                                .let { callback(it) }
+                        RESULT_TYPE.JSON -> response.responseBodyAsBytes?.let(JsonIterator::deserialize)
+                                .let(callback)
                         else -> throw IllegalArgumentException("Invalid function type: $resultType")
                     }
-                    return response
-                }
 
-                override fun onThrowable(t: Throwable) {
-                    t.printStackTrace() // TODO
-                }
+        }
 
-            }
+    }
 
-    // TODO Use Builders.blocking or Deferred.blockingAsync - https://github.com/Kotlin/kotlinx.coroutines/issues/79
     override fun execute(request: BoundRequestBuilder, resultType: RESULT_TYPE, callback: (Any?) -> Unit): Unit {
         request.execute(resultHandler(resultType, callback))
     }
