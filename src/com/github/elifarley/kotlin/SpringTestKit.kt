090f378e45117734a6a2abd1c4539394d90d017c
Elifarley C <elifarley@users.noreply.github.com>
2017-11-30 16:22:36 -0200

    Update SpringTestKit.kt

--- src/com/github/elifarley/kotlin/SpringTestKit.kt
+++ src/com/github/elifarley/kotlin/SpringTestKit.kt
@@ -2,23 +2,17 @@ package com.orgecc.test
 
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import java.io.File
-import java.util.*
 
 fun ResponseEntity<*>.exportToMap() = linkedMapOf(
             "http-status" to statusCode.name,
             "body" to body.toString()
     )
 
-fun ResponseEntity<*>.export(file: File, comments: String? = null) = Properties().also {
-    it.putAll(this.exportToMap())
-    it.store(file.outputStream(), comments)
-}
-
 fun toResponseEntity(m: Map<String, String>): ResponseEntity<String> = ResponseEntity(
         m["body"] ?: "",
         m["http-status"]?.let { HttpStatus.valueOf(it) } ?: HttpStatus.OK
 )
 
-inline fun callWithMockSupport(mockFilePropName: String, realCall: () -> ResponseEntity<String>): ResponseEntity<String> =
-        callWithMockSupport(mockFilePropName, ::toResponseEntity, realCall)
+open class CallWithMockSupportForResponseEntity(mockFilePropName: String): CallWithMockSupport<ResponseEntity<String>>(mockFilePropName) {
+    final override val importResultFromMapFun = ::toResponseEntity
+}
