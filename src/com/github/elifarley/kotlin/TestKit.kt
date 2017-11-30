362fca2ea4bdacce28ebe9b91f65b2c99d993ef8
Elifarley C <elifarley@users.noreply.github.com>
2017-11-30 16:20:07 -0200

    CallWithMockSupport: New class to replace old code

--- src/com/github/elifarley/kotlin/TestKit.kt
+++ src/com/github/elifarley/kotlin/TestKit.kt
@@ -1,51 +1,44 @@
 package com.orgecc.test
 
-import com.orgecc.logging.logger
+import com.orgecc.logging.WithLogging
 import java.io.File
 import java.io.FileInputStream
 import java.util.*
 
-abstract class MockFileHelper<in T>(private val mockFilePropName: String) {
+abstract class CallWithMockSupport<R>(private val mockFilePropName: String) {
 
-    private var mockTempFile: File? = null
+    companion object : WithLogging()
 
-    abstract val exportToMapFun: T.() -> Map<String, String>
-
-    fun setUp(mockResponse: T? = null) {
-        mockTempFile?.delete()
-        mockTempFile = mockResponse?.exportToMapFun()?.exportToTempFile()?.also { tmpFile ->
-            System.setProperty(mockFilePropName, tmpFile.canonicalPath)
+    var mockIsSet = false
+        private set
+    var mockResult: R? = null
+        set(value) {
+            field = value
+            mockIsSet = true
         }
+    fun resetMockResult() {
+        mockResult = null
+        mockIsSet = false
     }
 
-    fun tearDown() {
-        mockTempFile?.delete()
-        System.setProperty(mockFilePropName, "")
-    }
-}
+    protected abstract val importResultFromMapFun: Map<String, String>.() -> R
 
-fun Map<String, String>.exportToTempFile(comments: String? = null) = createTempFile(suffix = ".properties").also { tmp ->
-    val props = if (this is Properties)
-        this
-    else
-        Properties().also { newProps ->
-            newProps.putAll(this)
-        }
-    props.store(tmp.outputStream(), comments)
-}
+    fun call(realCall: () -> R): R? =
+            if (mockIsSet) {
+                LOG.error("[MOCK] Returning object: {}", mockResult)
+                mockResult
 
-fun <T> T.exportToTempFile(exportToMap: T.() -> Map<String, String>, comments: String? = null) =
-        this.exportToMap().exportToTempFile(comments)
+            } else System.getProperty(mockFilePropName, System.getenv(mockFilePropName)).let {mockFilePath ->
 
-inline fun <R> callWithMockSupport(mockFilePropName: String, noinline mockCall: Map<String, String>.() -> R, realCall: () -> R): R {
+                if (mockFilePath.isNullOrEmpty()) {
+                    return realCall()
+                }
 
-    val mockFilePath : String? = System.getProperty(mockFilePropName, System.getenv(mockFilePropName))
-    if (mockFilePath.isNullOrEmpty()) {
-        return realCall()
-    }
+                LOG.error("[MOCK: $mockFilePropName] Reading mocked response from '{}'...", mockFilePath)
+                importResultFromFile(mockFilePath)
+            }
 
-    logger(mockCall::class).error("[MOCK] Reading mocked response from '{}'...", mockFilePath)
-    return File(mockFilePath).let { mockFile ->
+    private fun importResultFromFile(mockFilePath: String?): R = File(mockFilePath).let { mockFile ->
         try {
 
             if (!mockFile.canRead()) {
@@ -54,7 +47,7 @@ inline fun <R> callWithMockSupport(mockFilePropName: String, noinline mockCall: 
 
             Properties().let {
                 it.load(FileInputStream(mockFile))
-                mockCall(it as Map<String, String>)
+                importResultFromMapFun(it as Map<String, String>)
             }
 
         } catch (e: IllegalArgumentException) {
